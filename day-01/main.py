82edcca3e829659000d6d154b26332afbca45aab
Jernej Janež <jernej.janez@mikrocop.com>
2021-12-07 15:34:37 +0100

    Day 1

---
+++ day-01/main.py
@@ -0,0 +1,38 @@
+def part_one(depths):
+    increases = 0
+    decreases = 0
+    previous_depth = None
+
+    for depth in depths:
+        current_depth = depth
+        if previous_depth is not None:
+            if current_depth > previous_depth:
+                increases += 1
+            else:
+                decreases += 1
+        previous_depth = current_depth
+
+    print("Increases: {}".format(increases))
+    print("Decreases: {}".format(decreases))
+
+
+def part_two(depths):
+    sliding_window = 3
+    three_measurement_depths = []
+
+    for i in range(len(depths) - sliding_window + 1):
+        three_measurement_depths.append(sum(depths[i: i + sliding_window]))
+
+    return three_measurement_depths
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as file:
+        lines = [int(line) for line in file]
+
+print("Part one:")
+part_one(lines)
+print()
+print("Part two:")
+three_measure_depths = part_two(lines)
+part_one(three_measure_depths)
